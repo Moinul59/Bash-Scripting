42c3b1701b0ce66a679cc58d4282f2d97193eb59
Sk Md Moinul Islam <moinul0459@gmail.com>
2025-05-04 22:17:06 +0530

    Add case and function practice scripts and update update-script

--- function-practice/update-script.sh
+++ function-practice/update-script.sh
@@ -4,15 +4,19 @@ release_file=/etc/os-release
 logfile=/var/log/updater.log
 errorlog=/var/log/updater_errors.log
 
+check_exit_status() {
+     if [ $? -ne 0 ]
+     then
+	 echo "An error occurred, please check the $errorlog file."
+     fi
+}
+
 # if [ -d /etc/pacman.d ]
 if grep -q "Arch" $release_file
 then
     # The host is based on Arch, run the pacman update command
     sudo pacman -Syu 1>>$logfile 2>>$errorlog
-    if [ $? -ne 0 ]
-    then
-	echo "An error occurred, please check the $errorlog file."
-    fi
+    check_exit_status
 fi
 
 # if [ -d /etc/apt ]
@@ -21,15 +25,9 @@ then
     # The host is based on Debian or Ubuntu,
     # Run the apt update command
     sudo apt update 1>>$logfile 2>>$errorlog
-    if [ $? -ne 0 ]
-    then
-        echo "An error occurred, please check the $errorlog file."
-    fi
+    check_exit_status
     sudo apt-dist upgrade 1>>$logfile 2>>$errorlog
-    if [ $? -ne 0 ]
-    then
-        echo "An error occurred, please check the $errorlog file."
-    fi
+    check_exit_status
 fi
 
 # if [ -d /etc/dnf ]
@@ -38,13 +36,7 @@ then
     # The host is based on RHEL
     # Run the dnf update command
     sudo dnf check-update 1>>$logfile 2>>$errorlog
-    if [ $? -ne 0 ]
-    then
-        echo "An error occurred, please check the $errorlog file."
-    fi
+    check_exit_status
     sudo dnf -y upgrade --refresh 1>>$logfile 2>>$errorlog
-    if [ $? -ne 0 ]
-    then
-        echo "An error occurred, please check the $errorlog file."
-    fi
+    check_exit_status
 fi
